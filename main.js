c4f88460dd71d22fe3fe11af7573af753a85e2c8
DoodleEvaHaHaHa <92247115+DoodleEvaHaHaHa@users.noreply.github.com>
2022-06-08 13:59:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+rightWristX = "";
+rightWristY = "";
+scoreRightWrist = "";
 /*created by prashant shukla */
 
 var paddle2 =10,paddle1=10;
@@ -21,17 +23,35 @@ var ball = {
     dy:3
 }
 
-function setup(){
+function setup()
+{
   var canvas = createCanvas(700,600);
   canvas.parent("canvas");
   video = createCapture(VIDEO);
   video.size(700,600);
   video.hide();
+
   poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    rightWristX = results[0].pose.rightWrist.x;
+	  rightWristY = results[0].pose.rightWrist.y;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+    console.log("rightwristX = "+rightwristX+" ,rightwristY = "+rightwristY+" ,scoreRightWrist = "+scoreRightWrist);
+  }
 }
 
-
 function draw(){
+
+  if(scoreRightWrist > 0.2){
+    fill("#FF0000");
+    stroke("#FF0000");
+    circle(rightWristX, rightWristY, 20)
+  }
  background(0); 
  image(video,0,0,700,600);
  
